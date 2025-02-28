8be417b1ef5ef4a443f71a92fd4d3c7dc75b8a77
Tobe0504 <ezimorahtobenna@gmail.com>
2025-02-28 14:33:28 +0100

    Added the new origins

--- index.js
+++ index.js
@@ -20,6 +20,25 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(function (req, res, next) {
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://insure-all-the-way-frontend.vercel.app",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
